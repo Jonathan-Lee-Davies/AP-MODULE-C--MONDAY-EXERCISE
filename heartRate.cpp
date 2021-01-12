b1bcf5e0885363cecb03a9d4dccef4c6f8b381ac
Jonathan-Lee-Davies <56265594+Jonathan-Lee-Davies@users.noreply.github.com>
2021-01-12 16:22:31 +0000

    Update heartRate.cpp

--- heartRate.cpp
+++ heartRate.cpp
@@ -1,21 +1,40 @@
-//INCOMPLETE, TO BE CONTINUED
-
 #include <iostream>
+
+bool isNumber(const std::string s)
+{
+    std::string::const_iterator it = s.begin();
+    while (it != s.end() && std::isdigit(*it)) ++it;
+    return !s.empty() && it == s.end();
+}
 
 int main () {
 
-  int age, heartRate, targetHR;
-  double intensity;
+  int age, restingHR, targetHR;
+  std::string ageInput;
 
-  std::cout << "Please enter your age: ";
-  std::cin >> age;
+
+  while(true) {
+  std::cout << "Please enter your age: " ; 
+  std::cin >> ageInput;
+
+  if (isNumber(ageInput)){
+      age = std::stoi(ageInput);
+      break;
+    }
+    std::cout << "Please enter a valid age value\n";
+  }
+
+
   std::cout << "Please enter your resting heart rate: ";
-  std::cin >> heartRate;
+  std::cin >> restingHR;
 
-  targetHR = (((220 - age) - heartRate) * intensity) + heartRate;
-  std::cout << targetHR;
- 
- std::cout << "\n" << "Resting pulse: " << heartRate << "  Age: " << age;
+  std::cout << "\nResting heart rate: " << restingHR << "\tAge: " << age ; 
 
- return 0;
+  for(float intensity = 0.4; intensity < 1; intensity += 0.05) {
+    targetHR = (((220 - age) - restingHR) * intensity) + restingHR;
+    
+    std::cout << "\n" << intensity << "%\t\t\t\t\t" << targetHR << "bpm";
+  }
+
+return 0;
 }
